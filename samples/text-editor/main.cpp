b90008ec0972e94ac3e0f9eaac69123bbae7ca90
Abel <abelcd@protonmail.com>
2018-04-14 18:09:22 +0200

    *

--- samples/text-editor/main.cpp
+++ samples/text-editor/main.cpp
@@ -1,32 +1,32 @@
 #include <iostream>
 
-#include "magui/Core/Window.hpp"
-#include "magui/Core/Application.hpp"
-#include "magui/Core/Color.hpp"
-#include "magui/Components/ColumnLayout.hpp"
-#include "magui/Components/RowLayout.hpp"
-#include "magui/Components/Title.hpp"
+#include "oggy/Core/Window.hpp"
+#include "oggy/Core/Application.hpp"
+#include "oggy/Core/Color.hpp"
+#include "oggy/Components/ColumnLayout.hpp"
+#include "oggy/Components/RowLayout.hpp"
+#include "oggy/Components/Heading.hpp"
 
 int main() {
-  magui::Application * app = new magui::Application();
+  oggy::Application * app = new oggy::Application();
 
-  magui::RowLayout * toolbar = new magui::RowLayout(25);
-  toolbar->setBackground(magui::Color(200,200,200,255));
+  oggy::RowLayout * toolbar = new oggy::RowLayout(25);
+  toolbar->setBackground(oggy::Color(200,200,200,255));
 
 
-  magui::RowLayout * main = new magui::RowLayout();
-  main->setBackground(magui::Color(0,255,0,255));
+  oggy::RowLayout * main = new oggy::RowLayout();
+  main->setBackground(oggy::Color(0,255,0,255));
 
-  magui::ColumnLayout * text = new magui::ColumnLayout();
-  text->setBackground(magui::Color(255,255,255,255));
+  oggy::ColumnLayout * text = new oggy::ColumnLayout();
+  text->setBackground(oggy::Color(255,255,255,255));
 
-  magui::ColumnLayout * sidebar = new magui::ColumnLayout(200);
-  sidebar->setBackground(magui::Color(40,40,40,255));
+  oggy::ColumnLayout * sidebar = new oggy::ColumnLayout(200);
+  sidebar->setBackground(oggy::Color(40,40,40,255));
 
-  magui::Title * title = new magui::Title("Sidebar");
+  oggy::Heading * title = new oggy::Heading("Sidebar");
 
-  magui::RowLayout * statusbar = new magui::RowLayout(20);
-  statusbar->setBackground(magui::Color(200,200,200,255));
+  oggy::RowLayout * statusbar = new oggy::RowLayout(20);
+  statusbar->setBackground(oggy::Color(200,200,200,255));
 
   app->document->root->addChild(toolbar);
   app->document->root->addChild(main);
